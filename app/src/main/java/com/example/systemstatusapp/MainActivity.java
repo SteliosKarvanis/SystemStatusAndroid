98382b39813881b18c95189ff6a55e8a95c37420
stelios <stelioscbranco@hotmail.com>
2024-04-06 21:35:34 -0300

    settings page

--- app/src/main/java/com/example/systemstatusapp/MainActivity.java
+++ app/src/main/java/com/example/systemstatusapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.systemstatusapp;
 
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,10 +13,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.systemstatusapp.databinding.ActivityMainBinding;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,16 +28,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fab_button_clicked(navController);
-            }
-        });
     }
 
     @Override
@@ -41,8 +39,22 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
-
-    public void fab_button_clicked(NavController navController){
-        navController.navigate(R.id.action_nav_home_to_nav_edithome);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            if (navController.getCurrentDestination().getId() != R.id.nav_edithome) {
+                navController.navigate(R.id.action_nav_home_to_nav_edithome);
+            }
+        }
+        else {
+            navController.popBackStack();
+        }
+        return true;
     }
 }
